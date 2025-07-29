a3729ef40519d6c2edba613763bdcf0fefd823e6
Victor Santilli <victor.bsantilli@gmail.com>
2025-07-29 20:46:14 -0300

    feat: create a shopee

---
+++ projeto-2-Carrinho-de-Compras-da-Shopee-com-Node/src/index.js
@@ -0,0 +1,11 @@
+import createItem from "./services/item.js";
+
+
+const cart = [];
+
+console.log("Welcome to the your Shopee Cart!!");
+
+const item1 = await createItem("mousepad", 20.99 , 1);
+const item2 = await createItem("mouse", 39.99 , 3);
+
+console.log(item2.subtotal())
---
+++ projeto-2-Carrinho-de-Compras-da-Shopee-com-Node/src/services/cart.js
@@ -0,0 +1,22 @@
+//quais açoes meu carrinho pode fazer
+
+//casos de uso
+// -> adicionar item no carrinho
+async function addItemT(userCart) {
+    
+}
+
+// -> deletar item do carrinho
+async function deleteItem(userCart, name) {
+    
+}
+
+// -> remover um item - diminui um item
+async function removeItem(userCart, index) {
+    
+}
+
+// -> calcular o total do carrinho
+async function calculateTotal(userCart) {
+    
+}
