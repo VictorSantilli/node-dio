c30a83dcae919210a782fd06a353a217c7d387c8
VictorSantilli <130602837+VictorSantilli@users.noreply.github.com>
2025-08-06 14:18:25 -0300

    feat: modulos de 7 e 8

---
+++ 07-npm-package-basic/src/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+import logSymbols from 'log-symbols';
+
+console.log(logSymbols.success, 'Finished successfully!');
+
+console.log(chalk.bgHex('#DEADED').underline("salve"))
